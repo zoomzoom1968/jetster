faa5be45731033383fbff1907bd4ab2046925e41
Jet Collado <jccollado2@gmail.com>
2017-05-07 03:09:09 +0000

    destroy and existing place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,6 +25,13 @@ class PlacesController < ApplicationController
   def update
   @place = Place.find(params[:id])
   @place.update_attributes(place_params)
+  redirect_to root_path
+  end
+  
+  def destroy
+  @place = Place.find(params[:id])
+  @place.destroy
+  redirect_to root_path
   end
 
   private
