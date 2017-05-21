5e5503f0563b53c15c2ef6568d598206eeb2695a
Jet Collado <jccollado2@gmail.com>
2017-05-21 21:34:32 +0000

    set up notification emails on the live server

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ApplicationMailer
-  default from: "no-reply@nomsterapp.com"
+  default from: "no-reply@jetster.com"
 
   def comment_added(comment)
     @place = comment.place
