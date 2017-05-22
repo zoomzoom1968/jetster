3dd5470d55aea07254445c5ff13ec28f9200314b
Jet Collado <jccollado2@gmail.com>
2017-05-22 00:27:48 +0000

    build user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :places do
     resources :comments, only: :create
   end
+  resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
